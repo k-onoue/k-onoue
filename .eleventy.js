067d01a9a796010c845d92a813712ebabde867e0
k-onoue <k.onoue0724@gmail.com>
2024-07-12 13:17:06 +0900

    save

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,20 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/assets');
 	eleventyConfig.addPassthroughCopy('src/css');
 	eleventyConfig.addWatchTarget('src/css');
 
-	eleventyConfig.addCollection('projects', (collection) => {
-		return collection.getFilteredByGlob('src/projects/*.md');
+	eleventyConfig.addCollection('posts', (collection) => {
+		return collection.getFilteredByGlob('src/posts/*.md');
 	});
 
 	eleventyConfig.addShortcode('year', () => {
 		return `${new Date().getFullYear()}`;
+	});
+
+	eleventyConfig.addFilter("readableDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toFormat("MMM dd, yyyy");
 	});
 
 	return {
@@ -17,4 +23,5 @@ module.exports = function (eleventyConfig) {
 			output: 'public',
 		},
 	};
+
 };
